0b9c32b32c013ea461f29fb87a7d0c20ac0a5cb5
VakrathundaVinayagar <83525114+VakrathundaVinayagar@users.noreply.github.com>
2021-05-02 13:05:34 +1000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,437 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-main-entrance",
+      "name": "main entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.02482383069216887,
+        "pitch": -0.43756152602717613,
+        "fov": 1.1295095994859854
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.02482383069216887,
+          "pitch": -0.43756152602717613,
+          "rotation": 0,
+          "target": "0-main-entrance"
+        },
+        {
+          "yaw": -0.031232109495139326,
+          "pitch": -0.20735075717668572,
+          "rotation": 0,
+          "target": "1-covid-station-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.02482383069216887,
+          "pitch": -0.43756152602717613,
+          "title": "Sri Vakrathunda Vinayagar Temple",
+          "text": "Basin Temple"
+        }
+      ]
+    },
+    {
+      "id": "1-covid-station-entrance",
+      "name": "Covid Station Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.8012428985211173,
+        "pitch": -0.005085389852066413,
+        "fov": 1.1295095994859854
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6884526791458079,
+          "pitch": 0.024039284158732244,
+          "rotation": 0,
+          "target": "2-flag"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-flag",
+      "name": "Flag",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.111814524715081,
+        "pitch": -0.01607206113534332,
+        "fov": 1.1295095994859854
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05606457783851937,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-covid-station-entrance"
+        },
+        {
+          "yaw": -1.663832734460808,
+          "pitch": 0.09880563993244351,
+          "rotation": 0,
+          "target": "8-vasantha"
+        },
+        {
+          "yaw": 1.6219319458832473,
+          "pitch": 0.2564042976398966,
+          "rotation": 0,
+          "target": "6-kalayan"
+        },
+        {
+          "yaw": 2.4175322256648286,
+          "pitch": 0.04459813831418025,
+          "rotation": 0,
+          "target": "7-leftside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1205619144166796,
+          "pitch": -0.5654243065438322,
+          "title": "Flag / Kodi",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.9373364351640863,
+          "pitch": -0.1030321138493342,
+          "title": "Yagasalai",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.6383337064603012,
+          "pitch": -0.2447515538505396,
+          "title": "Vasantha Mandapabam",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.7447685419242145,
+          "pitch": -0.043788560127769216,
+          "title": "Kalayana madapam",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.9634623368297,
+          "pitch": 0.010085657033013007,
+          "title": "Temple office",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-back-2",
+      "name": "Back 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.49144888746752,
+        "pitch": -0.04790561498657553,
+        "fov": 1.282552337385179
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0895403690689527,
+          "pitch": 0.0653647627590832,
+          "rotation": 0,
+          "target": "7-leftside"
+        },
+        {
+          "yaw": -1.4764049201797427,
+          "pitch": 0.2505083442429772,
+          "rotation": 0,
+          "target": "4-back"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-back",
+      "name": "Back",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.578994495337696,
+        "pitch": -0.14872038265995258,
+        "fov": 1.282552337385179
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4962272178999036,
+          "pitch": 0.23998088912428628,
+          "rotation": 0,
+          "target": "3-back-2"
+        },
+        {
+          "yaw": -1.0094347140077033,
+          "pitch": 0.025844513253847623,
+          "rotation": 0,
+          "target": "7-leftside"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-backexit",
+      "name": "BackExit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kalayan",
+      "name": "Kalayan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-leftside",
+      "name": "LeftSide",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8175523739212078,
+          "pitch": 0.020473864288193866,
+          "rotation": 0,
+          "target": "2-flag"
+        },
+        {
+          "yaw": -2.9154174189876194,
+          "pitch": 0.04394513764431629,
+          "rotation": 0,
+          "target": "4-back"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7414316898971878,
+          "pitch": -0.16007078461216295,
+          "title": "Siva Durgai",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.3947795744932776,
+          "pitch": -0.2116843233324719,
+          "title": "God",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.790983404893618,
+          "pitch": -0.20540491672312022,
+          "title": "Ganesha God",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.249762283370874,
+          "pitch": 0.019202167800969505,
+          "title": "God",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-vasantha",
+      "name": "Vasantha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Ganesha Temple Trail",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
